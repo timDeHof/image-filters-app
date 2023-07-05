a34d438ca23e6c4effacc83f6893ae38fdb8b3f5
timDeHof <this.is.tim.dehof@gmail.com>
2023-07-05 18:56:13 -0400

    feat(components.d.ts): add declaration for global component 'AppBG' feat(package.json): add 'unplugin-vue-components' and 'vite-plugin-fonts' dependencies feat(vite.config.ts): import and configure 'vite-plugin-fonts' and 'unplugin-vue-components' plugins

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,22 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { VitePluginFonts } from 'vite-plugin-fonts'
+import Components from 'unplugin-vue-components/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePluginFonts({
+      google: {
+        families: [
+          {
+            name: 'Quicksand'
+          }
+        ]
+      }
+    }),
+    Components()
   ],
   resolve: {
     alias: {
