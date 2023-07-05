a5a4a0e5891afc35dc9670a8a4e42b26e81c8661
timDeHof <this.is.tim.dehof@gmail.com>
2023-07-05 18:01:23 -0400

    chore(package.json): add autoprefixer, postcss, and tailwindcss dependencies feat(postcss.config.js): add postcss configuration file with tailwindcss and autoprefixer plugins chore(App.vue): update App.vue template with new design and add tailwindcss classes chore(main.ts): remove commented out code and unused imports feat(tailwind.config.js): add tailwindcss configuration file

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,23 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div
+    class="font-['Quicksand'] max-w-xl bg-white p-8 shadow-2xl rounded absolute m-auto left-0 right-0 mt-32"
+  >
+    <h1 class="text-center text-3xl text-indigo-700">Vue Filters</h1>
+    <div
+      class="text-center my-8 py-32 text-slate-700 italic rounded border-4 border-double border-slate-700/25"
+    >
+      Drag and drop image.
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="my-8">
+      <canvas width="448" height="448"></canvas>
+      <div class="text-white text-xl mt-4">
+        <div class="flex justify-center gap-4">
+          <button type="button" class="bg-pink-600 py-4 w-full">Filter</button>
+          <button type="button" class="bg-pink-600 py-4 w-full">Filter</button>
+          <button type="button" class="bg-pink-600 py-4 w-full">Filter</button>
+        </div>
+        <a class="bg-indigo-700 py-4 block w-full mt-2 text-center"> Download </a>
+      </div>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,11 @@ const app = createApp(App)
 app.use(createPinia())
 
 app.mount('#app')
+
+// let example = 5;
+
+// function sum(a: number, b: number): number {
+//   return a + b
+// }
+
+// console.log(sum(5, 2))
