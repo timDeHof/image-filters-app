a428c0a419f8a5f35355b1369ff593d5a0b9b41b
timDeHof <this.is.tim.dehof@gmail.com>
2023-07-05 19:43:26 -0400

    feat(components): add AppEditor and AppUpload components
    - Added `AppEditor` component to `components.d.ts` to declare its type.
    - Added `AppUpload` component to `components.d.ts` to declare its type.
    - Created `AppEditor.vue` component with canvas, filter buttons, and download button.
    - Created `AppUpload.vue` component with drag and drop functionality for image upload.
    - Added `upload` action to `image` store to handle image upload in `AppUpload.vue`.
    
    The new components `AppEditor` and `AppUpload` are added to the `App.vue` template for further implementation.

---
+++ src/components/AppEditor.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="my-8">
+    <canvas width="448" height="448"></canvas>
+    <div class="text-white text-xl mt-4">
+      <div class="flex justify-center gap-4">
+        <button
+          type="button"
+          class="py-4 w-full"
+          v-for="(filter, index) in filters"
+          :key="index"
+          :class="{
+            'bg-pink-600': store.filter !== filter,
+            'bg-green-600': store.filter === filter
+          }"
+          @click="store.filter = store.filter === filter ? '' : filter"
+        >
+          {{ filter }}
+        </button>
+      </div>
+      <a class="bg-indigo-700 py-4 block w-full mt-2 text-center"> Download </a>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useImageStore } from '@/stores/image'
+const store = useImageStore()
+
+const filters = ['oceanic', 'vintage', 'rosetint']
+</script>
--- src/stores/image.ts
+++ src/stores/image.ts
@@ -7,5 +7,15 @@ export const useImageStore = defineStore('image', {
   state: (): ImageState => ({
     file: null,
     filter: ''
-  })
+  }),
+  actions: {
+    upload(e: DragEvent) {
+      if (!e.dataTransfer) return
+      const tempFile = e.dataTransfer.files[0]
+
+      if (!tempFile.type.match('image.*')) return
+
+      this.file = tempFile
+    }
+  }
 })
