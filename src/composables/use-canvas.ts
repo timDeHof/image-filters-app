8b79cd9baf8b913dc4a61a02187d6efddbc247d4
timDeHof <this.is.tim.dehof@gmail.com>
2023-07-05 22:25:48 -0400

    feat(use-canvas.ts): add loadImage function to load canvas context
    The loadImage function is added to the useCanvas composable to load the canvas context. It checks if the canvas element exists and then assigns the canvas context to the canvasCtx variable.

--- src/composables/use-canvas.ts
+++ src/composables/use-canvas.ts
@@ -2,4 +2,10 @@ import { ref } from 'vue'
 
 export default function useCanvas() {
   const canvasEl = ref<HTMLCanvasElement | null>(null)
+  let canvasCtx: CanvasRenderingContext2D | null = null
+  function loadImage() {
+    if (!canvasEl.value) return
+
+    canvasCtx = canvasEl.value.getContext('2d')
+  }
 }
